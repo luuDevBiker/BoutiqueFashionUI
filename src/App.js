4729e327895e5f0bd445a42badf639f29cf5b36e
luuDevBiker <luubiker@gmail.com>
2022-05-26 12:50:01 +0700

    create-UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./module_view_web/home";
+import Header from "./module_view_web/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/module_view_web/header.jsx
@@ -0,0 +1,12 @@
+import { ReactDOM } from "react";
+
+const Header = () => {
+    return (
+        <div>
+        <h1>Header</h1>
+        </div>
+    );  
+}
+
+export default Header;
+
