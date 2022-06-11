ca61c460be5f602b3e3d03ae225757d36d6023af
Lê Văn Lưu <87215931+luuDevBiker@users.noreply.github.com>
2022-06-11 15:20:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import ForgetPass from "./components/forgetPass";
 import Login from "./components/login";
 import Register from "./components/register";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { TableComponent } from "./components/table/table";
+import { ProductDetail } from "./components/productDetail";
+import { Cart } from "./components/cart/Cart";
+import { Payment } from "./components/payment/Payment";
+import Header from"./components/Header/header";
+import Footer from"./components/Footer/footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forget-password" element={<ForgetPass />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forget-password" element={<ForgetPass />} />
+        <Route path="/table" element={<TableComponent />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/footer" element={<Footer/>}/>
+      </Routes>
       </BrowserRouter>
     </div>
   );
